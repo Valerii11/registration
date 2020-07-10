f1ae606f5654a343fe86901b0a5b9205a6d0682c
Valerii <Mixaluch11@i.ua>
2020-07-10 21:15:38 +0300

    Код форми реєстрації

--- reg.js
+++ reg.js
@@ -1,8 +1,24 @@
 'use strict';
 
-
-
 window.addEventListener('load', () => {
 
+//    let form = document.getElementById('form1');
+//    form.addEventListener('submit', () => {
+//        
+//        
+//      let access = confirm('Do you agree?');
+//        if (access === true) {
+//            form.submit();
+//        } else {
+//            alert('Change password!');
+//        }
+//    });
     
-})
+    const controller = new Controller('login', 'pass1', 'pass2', 'email', 'form1');
+    controller.activateLiseners();
+    
+    
+    
+    
+    
+});
